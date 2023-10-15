ac70b63ee4c920c516ab4f4fc2744983d579adf2
Azim Sofi <azimmiskandar@gmail.com>
2023-10-15 15:58:10 +0800

    Ping .txt to .xlsx

---
+++ ping.py
@@ -0,0 +1,21 @@
+import re
+import pandas as pd
+
+def process_ping_data(input_txt_path, output_excel_path):
+    with open(input_txt_path, 'r', encoding='utf-16') as f:
+        data = f.read()
+        
+    time_values = re.findall(r'time=(\d+)ms', data)
+    time_values = list(map(int, time_values))
+    
+    df = pd.DataFrame({'Time(ms)': time_values})
+    df.to_excel(output_excel_path, index=False)
+
+input_file = input('Input file name: ')
+input_txt_path = f"{input_file}.txt"
+
+output_file = input('Output file name: ')
+output_excel_path = f"{output_file}.xlsx"
+
+# Process the data and save to Excel
+process_ping_data(input_txt_path, output_excel_path)
